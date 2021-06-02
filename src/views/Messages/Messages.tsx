c6dc24c2612797790113a476ad4b53dc8e989257
Alex Shorsher <alex.shorsher@kaleido.io>
2021-06-02 13:26:25 -0400

    add license header

--- src/views/Messages/Messages.tsx
+++ src/views/Messages/Messages.tsx
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 import React, { useState, useEffect, useContext } from 'react';
 import {
   Grid,
