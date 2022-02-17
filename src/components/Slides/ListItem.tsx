54d316102022797482cba4c4628702381b90ad76
David Echelberger <eberger727@gmail.com>
2022-02-17 13:29:46 -0500

    [v1-dashboard] new slide, tables, and timeline components
    Signed-off-by: David Echelberger <eberger727@gmail.com>

---
+++ src/components/Slides/ListItem.tsx
@@ -0,0 +1,34 @@
+import { Grid, Typography } from '@mui/material';
+
+export interface IDataListItem {
+  label: string;
+  value: string | JSX.Element;
+  button?: JSX.Element;
+}
+
+interface Props {
+  item: IDataListItem;
+}
+
+export const DrawerListItem: React.FC<Props> = ({ item }) => {
+  return (
+    <Grid xs={12} py={1} container item alignItems="center">
+      <Grid item xs={2}>
+        <Typography color="secondary" sx={{ fontSize: 12 }}>
+          {item.label}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={8}
+        color="primary"
+        sx={{ fontSize: 12, fontWeight: 'bold' }}
+      >
+        {item.value}
+      </Grid>
+      <Grid item xs={2}>
+        {item.button}
+      </Grid>
+    </Grid>
+  );
+};
---
+++ src/components/Slides/Panel.tsx
@@ -0,0 +1,62 @@
+import { Grid, Paper, Typography } from '@mui/material';
+import { getShortHash } from '../utils';
+
+export interface IOperationListItem {
+  label: string;
+  value: string;
+  badge?: JSX.Element;
+  button?: JSX.Element;
+  onClick?: () => void;
+}
+
+interface Props {
+  item: IOperationListItem;
+}
+
+export const DrawerPanel: React.FC<Props> = ({ item }) => {
+  return (
+    <Paper
+      sx={{
+        width: '100%',
+        height: '100%',
+        p: 1,
+        mb: 1,
+      }}
+      elevation={0}
+    >
+      <Grid xs={12} p={1} container>
+        <Grid item xs={6} direction="column">
+          <Typography
+            color="primary"
+            sx={{ fontSize: 14, pb: 1, fontWeight: 'bold' }}
+          >
+            {item.label}
+          </Typography>
+          <Typography color="secondary" sx={{ fontSize: 12 }}>
+            {getShortHash(item.value)}
+          </Typography>
+        </Grid>
+        <Grid
+          alignItems="center"
+          justifyContent="flex-end"
+          container
+          item
+          xs={4}
+          color="primary"
+          sx={{ fontSize: 12, fontWeight: 'bold' }}
+        >
+          {item.badge}
+        </Grid>
+        <Grid
+          container
+          item
+          xs={2}
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          {item.button}
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
